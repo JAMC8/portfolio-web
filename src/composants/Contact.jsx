2deadbdbe927816948d68a60ffbb17c03842b801
Julian Maldonado <juliandresmc@hotmail.com>
2021-02-25 11:27:01 -0500

    update contact

--- src/composants/Contact.jsx
+++ src/composants/Contact.jsx
@@ -8,18 +8,20 @@ export default function Contact(props)
             <MenuSecondaire />
             <div className="ContenuPrincipal">
                 <div className="Contact">
+                    <h2 className="titrePage">Contact</h2>
+                    <div className="info">
+                        <div className="courriel">
+                            <h3>Adresse courriel</h3>
+                            <a href="mailto:juliandresmc@hotmail.com">juliandresmc@hotmail.com</a>
+                        </div>
+                        <div className="telephone">
+                            <h3>Téléphone</h3>
+                            <a href="tel:5145016353">(514) 501-6353</a>
+                        </div>
+                    </div>            
                     <div className="logo">
                         <img src='images-logo/LogoNom.png'/>
                     </div>
-                    <h2 className="titrePage">Contact</h2>
-                    <div>
-                        <h3>Adresse courriel</h3>
-                        <a href="mailto:juliandresmc@hotmail.com">juliandresmc@hotmail.com</a>
-                    </div>
-                    <div>
-                        <h3>Téléphone</h3>
-                        <a href="tel:5145016353">(514) 501-6353</a>
-                    </div>            
                 </div>
             </div>
         </div>

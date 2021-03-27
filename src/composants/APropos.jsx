cbe243107ceafba46351acbc951b7b93434b5e7c
Julian Maldonado <juliandresmc@hotmail.com>
2021-03-27 19:55:36 -0400

    correction

--- src/composants/APropos.jsx
+++ src/composants/APropos.jsx
@@ -13,7 +13,7 @@ export default function APropos(props)
                 <div className="textePresentation">
                     <p>Bonjour, je m'appelle <span className="nom">Julian Maldonado</span>,</p>
                     <p>Je suis finissant en Techniques d'Intégration Multimédia au Collège de Maisonneuve. Je suis passionné par la programmation C# et mon objectif est de devenir programmeur de jeux vidéo. Je veux faire la différence dans le milieu du jeu vidéo.</p>
-                    <p>Mon principal champ d’intérêt est surtout par la programmation de jouabilité des jeux vidéo mais j'aime aussi la programmation Web back-end.</p>
+                    <p>Mon principal champ d’intérêt est surtout la programmation de jouabilité des jeux vidéo mais j'aime aussi la programmation Web back-end.</p>
                     <p>Mon plan d’action après avoir fini mon D.E.C en intégration multimédia c’est d’aller à l’université ETS dans le programme de Génie Logiciel. Pour mon futur, je veux travailler pour des compagnies de jeux vidéos d'haute gamme comme Ubisoft ou EA.</p>
                 </div>
 
@@ -95,13 +95,6 @@ export default function APropos(props)
                 </div>
                 <br />
                 <br />
-
-                {/*<div className="telechargerCV">
-                    <h3>Mon CV:</h3>
-                    <button>Téléchrger CV</button>
-                    <span>(Pas encore fonctionel)</span>
-                </div>*/}
-
             </div>
         </>
     );

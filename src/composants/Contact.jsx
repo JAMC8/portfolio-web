2cbe3a84106994070d64693537acd095484c3f7b
Julian Maldonado <julian.maldonado@arvisiongames.com>
2022-03-08 17:32:14 -0500

    Créations divition

--- src/composants/Contact.jsx
+++ src/composants/Contact.jsx
@@ -27,6 +27,8 @@ export default function Contact(props)
                         <div className="courriel">
                             <h3>Adresse courriel</h3>
                             <a href="mailto:juliandresmc@hotmail.com">juliandresmc@hotmail.com</a>
+                        </div>
+                        <div className="courriel">
                             <h3>Adresse courriel ÉTS</h3>
                             <a href="mailto:julian-andres.maldonado.1@ens.etsmtl.ca">julian-andres.maldonado.1@ens.etsmtl.ca</a>
                         </div>
